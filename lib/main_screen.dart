5f6e20ce76f26fce1b0b3d2c9e6fc3599ed4e960
Hassan Ayoub Zafar <hz.asd1@gmail.com>
2021-10-06 03:22:25 +0500

    .

---
+++ lib/main_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class MainScreens extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      children: [
+        // BottomBarScreen(), UploadProductForm()
+        ],
+    );
+  }
+}
