324004f71016d593ce32d598ea352f54b58f9417
Hassan Ayoub Zafar <60160952+hassan-zafar@users.noreply.github.com>
2021-10-16 05:34:52 +0500

    removed most videos

---
+++ lib/consts/consants.dart
@@ -0,0 +1,18 @@
+//categories folder
+String thinking = "assets/videos/Thinking";
+String daily = "assets/videos/Daily";
+String education = "assets/videos/Education";
+String movement = "assets/videos/Movement";
+String seated = "assets/videos/Seated";
+
+String thinking1stVid = "$thinking/Self Value.mp4";
+// "Conscious calming connecting mediation.mp4"
+// "Quick wealth.mp4"
+// "Empowering mediation.mp4"
+// "Overcoming struggles.mp4"
+// "Confident can do.mp4"
+
+String movement1stVid = "$movement/Movement Running Meditation.mp4";
+String seated1stVid = "$seated/Charkra Clearing Meditation.mp4";
+String education1stVid = "$education/Letting go of Negativity.mp4";
+String daily1stVid = "$daily/Evening mediation.mp4";
