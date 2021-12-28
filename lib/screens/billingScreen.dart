e02f14fb307a71f8353a704115cffd5a2d0fc933
Hassan Ayoub Zafar <60160952+hassan-zafar@users.noreply.github.com>
2021-12-29 00:27:06 +0500

    Update billingScreen.dart

--- lib/screens/billingScreen.dart
+++ lib/screens/billingScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meditation_alive/consts/collections.dart';
+import 'package:meditation_alive/consts/consants.dart';
 
 class BillingScreen extends StatefulWidget {
   static const routeName = '/BillingScreen';
@@ -11,17 +12,29 @@ class BillingScreen extends StatefulWidget {
 }
 
 class _BillingScreenState extends State<BillingScreen> {
+  DateTime expiryDate = DateTime.parse(currentUser!.subscriptionEndTIme!);
   @override
   Widget build(BuildContext context) {
+    String formattedtime =
+        '${expiryDate.day}/${expiryDate.month}/${expiryDate.year}';
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Billing'),
         centerTitle: true,
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Text('Monthly Subscription'),
-          Text('Next billing date is ${currentUser!.subscriptionEndTIme}')
+          Text(
+            'Monthly Subscription',
+            style: titleTextStyle(color: Colors.white),
+          ),
+          Text(
+            'Next billing date is $formattedtime',
+            style: titleTextStyle(color: Colors.white, fontSize: 22),
+          )
         ],
       ),
     );
