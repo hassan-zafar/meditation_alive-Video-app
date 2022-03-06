bc669e57918b830eb133eb47b5ca22e195f40884
Hassan Ayoub Zafar <60160952+hassan-zafar@users.noreply.github.com>
2022-03-07 00:46:41 +0500

    .

--- lib/main.dart
+++ lib/main.dart
@@ -151,9 +151,6 @@ class _MyAppState extends State<MyApp> {
                   home: UserState(),
                   //initialRoute: '/',
                   routes: {
-                    // '/': (ctx) => LandingPage(),
-                    // WebhookPaymentScreen.routeName: (ctx) =>
-                    //     WebhookPaymentScreen(),
                     WishlistScreen.routeName: (ctx) => WishlistScreen(),
                     MainScreens.routeName: (ctx) => MainScreens(),
                     LoginScreen.routeName: (ctx) => LoginScreen(),
@@ -170,59 +167,3 @@ class _MyAppState extends State<MyApp> {
         });
   }
 }
-
-
-
-// import 'package:volt_arena/screens/home_screen.dart';
-// import 'package:volt_arena/screens/introduction_auth_screen.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:firebase_core/firebase_core.dart';
-// import 'package:flutter/material.dart';
-
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await Firebase.initializeApp();
-//   runApp(MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       home: NavigationPage(),
-//     );
-//   }
-// }
-
-// class NavigationPage extends StatefulWidget {
-//   @override
-//   _NavigationPageState createState() => _NavigationPageState();
-// }
-
-// class _NavigationPageState extends State<NavigationPage> {
-//   bool isLoggedIn = false;
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     FirebaseAuth.instance.authStateChanges().listen((event) {
-//       if (event != null) {
-//         setState(() {
-//           isLoggedIn = true;
-//         });
-//       } else {
-//         setState(() {
-//           isLoggedIn = false;
-//         });
-//       }
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: isLoggedIn == false ? IntroductionAuthScreen() : HomeScreen(),
-//     );
-//   }
-// }
